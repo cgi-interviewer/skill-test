523b5c124d8836dd44ea15ea41d9b97d1bad3023
Zoltan Papp <zoltankpapp@gmail.com>
2023-02-24 22:01:07 +0100

    implementing todos

--- src/main/java/com/cgi/boat/interview/PeopleProcessor.java
+++ src/main/java/com/cgi/boat/interview/PeopleProcessor.java
@@ -2,40 +2,34 @@ package com.cgi.boat.interview;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class PeopleProcessor {
     /**
-     * Returns a {@link Map} where keys are first names and values lists of all last names
-     * of people from the input list who have the first name
-     * equal to the key.
      *
-     * Example:
-     * For input: ["John Doe", "John Silver", "Peter Doe"]
-     * Expected result would be:
-     * {
-     *  "John" -> ["Doe", "Silver"]
-     *  "Peter" -> ["Doe"]
-     * }
+     * @param people List of Person objects
+     * @return       Map with Person's firstName attribute as key
+     *               and a String list as value, which contains all the last names
+     *               which can connect with the key as a first name last name pair
      */
     static Map<String, List<String>> lastnamesByFirstname(List<Person> people){
-        //TODO: implement
+        return people
+                .stream()
+                .collect(Collectors.groupingBy(Person::getFirstName,
+                        Collectors.mapping(Person::getLastName, Collectors.toList())));
     }
-
 
     /**
-     * Same as {@link PeopleProcessor#lastnamesByFirstname} except that the mapping
-     * returned is lastname -> firstnames
      *
-     * Example:
-     * For input: ["John Doe", "John Silver", "Peter Doe"]
-     * Expected result would be:
-     * {
-     *  "Doe" -> ["John", "Peter"]
-     *  "Silver" -> ["John"]
-     *
+     * @param people List of Person objects
+     * @return       Map with Person's lastName attribute as key
+     *               and a String list as value, which contains all the first names
+     *               which can connect with the key as a first name last name pair
      */
     static Map<String, List<String>> firstnamesByLastname(List<Person> people){
-        //TODO: implement
+        return people
+                .stream()
+                .collect(Collectors.groupingBy(Person::getLastName,
+                        Collectors.mapping(Person::getFirstName, Collectors.toList())));
     }
-
 }
